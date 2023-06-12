b01a8343522cb3de9dcb8a1d28f9d91312b09344
Lance A. Endres <lanceendres@gmail.com>
2023-06-12 09:54:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Introduction
+This repository contains the source files for the _Monograph on Drilling Dynamics and Mechanics_.
---
+++ src/run_manual_make_document.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem Use this file to process the document without WinEdt.
+rem It can be double clicked or run from the command line.
+rem See "manual_make_document" for details about how it works.
+
+call manual_make_document
